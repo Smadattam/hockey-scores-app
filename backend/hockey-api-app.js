f0782bf0a2a78e53cbd01a2d2180be6537cf00c7
smadattam <matthewadams623@gmail.com>
2021-03-14 13:24:05 -0700

    Test: Commented out cors code

--- backend/hockey-api-app.js
+++ backend/hockey-api-app.js
@@ -2,7 +2,7 @@
 
 const express = require('express'); // import express module (simplifies routing/requests, among other things)
 const app = express(); // create an instance of the express module (app is the conventional variable name used)
-const cors = require('cors');  // import cors module from express
+// const cors = require('cors');  // import cors module from express
 const fetch = require('node-fetch'); // import node-fetch (enables the fetch API to be used server-side)
 const PORT = process.env.PORT || 5000; // use either the host env var port (PORT) provided by Heroku or the local port (5000) on your machine
 
